73b69e5f327f8a35d917874f50e1912e60549ec0
Yongho-VIRNECT <yhkim@virnect.com>
2021-06-30 09:09:24 +0900

    전세계 코로나 요약 정보 API 함수 반환 타입 모듈화 및 구체화
    - 빌드 오류 수정

--- project/src/app.ts
+++ project/src/app.ts
@@ -1,5 +1,6 @@
-// import axios from "axios";
-// import * as Chart  from "chart.js";
+import axios, { AxiosResponse } from "axios";
+import * as Chart from "chart.js";
+import { CovidSummaryResponse } from './covid/index';
 // utils
 function $(selector: string) {
   return document.querySelector(selector);
@@ -39,8 +40,10 @@ function createSpinnerElement(id: string) {
 let isDeathLoading = false;
 const isRecoveredLoading = false;
 
+
+
 // api
-function fetchCovidSummary() {
+function fetchCovidSummary(): Promise<AxiosResponse<CovidSummaryResponse>> {
   const url = 'https://api.covid19api.com/summary';
   return axios.get(url);
 }
